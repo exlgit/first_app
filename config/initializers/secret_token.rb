17cd1d8791301c8c1679b6a6a28cacfb974b4d56
Tanner Hiland <tanner@exoliamethod.com>
2010-12-02 23:58:52 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '93539826a83e4735c14130b73ef8a45419fe72b62b4edc7176b43792c21a0abe9a9a5a5c01ec475fe7068ac27ee9d606bcc116c5ab66800e3ad16876034c6923'
